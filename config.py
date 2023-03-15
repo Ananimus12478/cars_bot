5c93975a8cb36a3f1acc8d2495b2a39e0f3ec34f
Nurlan <militory290@gmail.com>
2023-03-15 19:56:42 +0600

    first commit

---
+++ config.py
@@ -0,0 +1,13 @@
+TOKEN = "6037413508:AAGyScVhUxu8RGlIz40kRq7EyoQb3C0QCb8"
+
+
+DATABASE_USER = "root"
+DATABASE_PASSWORD = "qwerty123"
+DATABASE_HOST = "localhost"
+DATABASE_NAME = "car_db"
+
+# Ссылка на базу данных
+MYSQL_URL = f"mysql+pymysql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_NAME}"
+
+
+
